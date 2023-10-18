3df595ecef73456b837b831a5d737e4ac964ffd9
cheiru94 <cheiru94@gmail.com>
2023-10-18 17:28:38 +0900

    useRef study

--- ichibansreact/src/index.js
+++ ichibansreact/src/index.js
@@ -1,74 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 
-// useState, useEffect 예시
-
-const Info = (props) => {
-  const [name, setName] = useState('');
-  const [pwd, setPwd] = useState('');
-
-  useEffect(
-    () => {
-      console.log('렌더링 완료됨');
-      console.log(
-        { name, pwd }
-        // {name:name, pwd:pwd}
-      );
-      // 로그인처리---> 성공하면
-      return (  // cleanup
-        () => {
-          console.log('뒷처리');
-          console.log(
-            { name, pwd }
-          );
-        }
-      );
-    }
-    //,[]
-    , [name]
-  );
 
 
-  const changeName = (e) => {
-    setName(e.target.value);
-  };
-  const changePwd = (e) => {
-    setPwd(e.target.value);
-  };
-  return (<>
-    <input value={name} onChange={changeName} />
-    <input value={pwd} onChange={changePwd} />
-    <hr />
-    <div>
-      <div><b>이름:</b>{name}</div>
-    </div>
-    <div>
-      <div><b>패스워드:</b>{pwd}</div>
-    </div>
-
-  </>);
-};
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
+  const [n, setN] = useState(0);
+
+  // 렌더링 되지 않는다.( 근데 안에 설정한 로직은 움직이고 있다.)
+  const todoIdx = useRef(0); // todoIdx = { current:0 } 의 의미가 된다
+  console.log(todoIdx);
+
+
+  /* 1 */
+  // useEffect(() => {
+  //   console.log('렌더링 했음');
+  // });
+
+  /* 2 */
+  useEffect(() => {
+    console.log('렌더링 했음');
+  }, [n]);
+
+  /* 3 */
+  useEffect(() => {
+    console.log('렌더링 했음');
+  }, [todoIdx.current]);
+
+
+  const addTodo = () => {
+    todoIdx.current = todoIdx.current + 1;
+    // console.log(todoIdx);
+    console.log(todoIdx.current);
+  }
+
+
   return (
     <>
-      <div
-        onClick={
-          () => {
-            setVisible(!visible);
-          }
-        }
-      >
-        <button>
-          {visible ? '숨기기' : '보이기'}
-        </button>
-      </div>
-      <hr />
-      {visible && <Info />}
+      <h1>카운터: {n}</h1>
+      <button onClick={() => setN(n + 1)} > +1 </button>
+      <hr></hr>
+
+      <h1>todoIdx:{todoIdx.current}</h1>
+      <button onClick={addTodo} > 새로할 일 추가 </button>
     </>
   )
-
 };
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
